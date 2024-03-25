8684d14f7b030847c7d0a4cbca3d449d2672ce5f
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-25 06:50:10 +0000

    feat: update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { QrcodeSuccessCallback } from "html5-qrcode";
+import HTML5QRCodePlugin from "./components/HTML5QRCodePlugin";
+
 export default function Home() {
+  const onNewScanResult: QrcodeSuccessCallback = (
+    decodedText,
+    decodedResult,
+  ) => {
+    alert(decodedText);
+  };
+
   return (
-    <main />
+    <main>
+      <HTML5QRCodePlugin
+        fps={60}
+        qrbox={480}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
+    </main>
   );
 }
